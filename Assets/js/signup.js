5473b32bcc8986b3a0b9d7b1c7ef00bf8094e8a9
Sharon2408 <sharon2408@gmail.com>
2023-05-20 19:30:28 +0530

    2nday commit

--- Assets/js/signup.js
+++ Assets/js/signup.js
@@ -1,54 +1,75 @@
-document.getElementById("signup-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    validateForm();
-  });
+// Empty Fields
+function validate(){
+  const nameInput = document.getElementById("name").value;
+  const emailInput = document.getElementById("email").value;
+  const passwordInput = document.getElementById('password').value;
+  const confirmpasswordInput = document.getElementById('confirmpassword').value;
+if(!nameInput || !emailInput ||!passwordInput || !confirmpasswordInput){
+  document.getElementById("nameError").innerHTML =
+      "Name cannot be empty";
+      document.getElementById("emailError").innerHTML =
+      "Email-ID cannot be empty";
+      document.getElementById("passwordError").innerHTML =
+      "Password cannot be empty";
+      document.getElementById("confirmpasswordError").innerHTML =
+      "Confirm Password cannot be empty";
+}
+}
+
+
+// Name Validation
+function validate_name() {
+  const nameInput = document.getElementById("name").value;
+  const nameregex = /^[a-zA-Z\- ]{3,50}$/;
+ 
+  if (!nameInput) {
+    document.getElementById("nameError").innerHTML =
+      "Name cannot be empty";
   
-  function validateForm() {
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-    var confirmPassword = document.getElementById("confirm-password").value;
-    
-    var errorMessages = [];
-    
-    if (name === "") {
-      errorMessages.push("Name is required.");
-    }
-    
-    if (email === "") {
-      errorMessages.push("Email is required.");
-    } else if (!isValidEmail(email)) {
-      errorMessages.push("Email is invalid.");
-    }
-    
-    if (password === "") {
-      errorMessages.push("Password is required.");
-    }
-    
-    if (confirmPassword === "") {
-      errorMessages.push("Confirm Password is required.");
-    } else if (confirmPassword !== password) {
-      errorMessages.push("Passwords do not match.");
-    }
-    
-    var errorContainer = document.getElementById("error-container");
-    errorContainer.innerHTML = "";
-    
-    if (errorMessages.length > 0) {
-      for (var i = 0; i < errorMessages.length; i++) {
-        var errorElement = document.createElement("p");
-        errorElement.innerText = errorMessages[i];
-        errorContainer.appendChild(errorElement);
+  } else if (!nameregex.test(nameInput)) {
+    document.getElementById("nameError").innerHTML =
+      "Name Should be atleast 3 charachters long";
+  }
+  else document.getElementById("nameError").innerHTML = "";
+}
+
+// Email Validation
+function validate_email() {
+  const emailInput = document.getElementById("email").value;
+  const emailregex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailInput) {
+    document.getElementById("emailError").innerHTML =
+      "Email-ID cannot be empty";
+  } 
+  else if (!emailregex.test(emailInput)) {
+    document.getElementById("emailError").innerHTML =
+      "Please enter a valid email-id";
+  }
+  else document.getElementById("emailError").innerHTML = "";
+}
+// PasswordValidation
+function validate_password(){
+  const passwordInput = document.getElementById('password').value;
+  const passwordregex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_\-+=<>?])[A-Za-z\d!@#$%^&*()_\-+=<>?]{8,}$/;
+  if (!passwordInput) {
+    document.getElementById("passwordError").innerHTML =
+      "Password cannot be empty";
+  }
+      else if(!passwordregex.test(passwordInput)){
+          document.getElementById('passwordError').innerHTML = "Password must contain 1 uppercase 1 lowercase and special symbols";
       }
-    } else {
-      // Form is valid, you can perform further actions here
-      alert("Sign up successful!");
-      document.getElementById("signup-form").reset();
-    }
+        else document.getElementById("passwordError").innerHTML = "";
+}
+// Confirm Password Validation
+function validate_confirmpassword(){
+  const confirmpasswordInput = document.getElementById('confirmpassword').value;
+  const passwordInput = document.getElementById('password').value;
+  if (!confirmpasswordInput ) {
+    document.getElementById("confirmpasswordError").innerHTML =
+      "Confirm Password cannot be empty";
   }
-  
-  function isValidEmail(email) {
-    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-  }
-  
+      else if(confirmpasswordInput !== passwordInput){
+          document.getElementById('confirmpasswordError').innerHTML = "Passwords not matched";
+      }
+        else document.getElementById("confirmpasswordError").innerHTML = "";
+}
