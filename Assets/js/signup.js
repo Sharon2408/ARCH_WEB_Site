19db03675dac01bbcc312781cf21c78ff07c4602
Sharon2408 <sharon2408@gmail.com>
2023-05-22 23:49:40 +0530

    api fetch

--- Assets/js/signup.js
+++ Assets/js/signup.js
@@ -110,114 +110,25 @@ function create_cards(){
   xhttp.open("GET", "http://localhost:3000/Cards");
   xhttp.send();
 xhttp.onreadystatechange = function (){
+  if (this.readyState == 4 && this.status == 200) {
   console.log(this.responseText)
-  var card =
+  var card = "";
+  const objects = JSON.parse(this.responseText);
+  for (let object of objects) {
+card += '<div class="col-md-4">';
+card +='<div class="card mb-4 glowing-border">';
+card +='<img src="'+ object["Photo"]+'" class="card-img-top" alt="Architecture 1">';
+card += '<div class="card-body">';
+card +="<h5 class='card-title'>" + object["Price"] + "</h5>";
+card += '<p class="card-text">'+object["Description"]+ '</p>';
+card += '<a href="#" class="btn btn-primary glowing-button">Buy Now</a>';
+card +=  '<address class="card-foot"><br><i class="fa-solid fa-location-dot"></i>'+object["Location"]+'</address>';
+card += '</div>';
+card += '</div>';
+card += '</div>';
 }
-
+document.getElementById('dynamiccard').innerHTML = card;
 }
-
-// const product = [];
-// const arr1 = [12, 6, 4];
-// const arr2 = [1, 2, 3];
-
-// for (let i = 0; i < arr1.length; i++) {
-//   const prod = arr1[i] * arr2[i];
-//   product.push(prod);
-// }
-
-// console.log(product);
-// var res;
-
-// const result = [];
-// const arr3 = [12,6,4]
-// const arr4 = [1,2,3]
-// for(let i = 0; i<=arr3.length;i++)
-// {
-//   for(let j=0;j<=arr4.length;j++)
-//   {
-//      res = arr3[i]*arr4[j]
-//     result.push(res) 
-//   }
-// }
-// console.log(result)
-
-// const str = ["hari","Ravi","Olive","Harsh"];
-// const newname = [];
-// const oldname = [];
-// var add;
-// for(let i = 0; i < str.length; i++)
-// {
-//   if(str[i].length < 5)
-//   {
-//     add = str[i]  + "Mr";
-//     newname.push(add);
-//   }
-//   else {
-//         oldname.push(str[i]);
-//       }
-// }
-
-// const str = ["hari", "Ravi", "Olive", "Harsh"];
-// const newname = [];
-// const oldname = [];
-// for (let i = 0; i <= str.length; i++) {
-//   if (str[i].length < 5) {
-//     add = str[i] + "Mr";
-//     newname.push(add);
-//   } else {
-//     oldname.push(str[i]);
-//   }
-// }
-// console.log(newname); // Output: ["hariMr", "OliveMr"]
-// console.log(oldname); // Output: ["Ravi", "Harsh"]
-
-// const str = ["hari", "Ravi", "Olive", "Harsh"];
-
-
-
-// const newname = [];
-// const oldname = [];
-
-// for (let i = 0; i < str.length; i++) {
-//   if (str[i].length < 5) {
-//     add = str[i] + "Mr";
-//     newname.push(add);
-//   } else {
-//     oldname.push(str[i]);
-//   }
-// }
-// console.log(newname); // Output: ["hariMr", "RaviMr"]
-//console.log(oldname); // Output: ["Olive", "Harsh"]
-
-// const arr_test = [15,13,14,18,12,11];
-// var a;
-// for (let i = 0; i<=arr_test.length;i++)
-// {
-//   for(let j = i+1; j<arr_test.length;j++)
-//   if(arr_test[i] > arr_test[j])
-//   {
-//      a = arr_test[i];
-//      arr_test[i] = arr_test[j];
-//      arr_test[j] = a;
-// }
-
-// }
-// console.log(arr_test)
-
-// const arr_test1 =[15,13,14,18,12,11];
-// var a;
-// for (let i = 0; i<=arr_test1.length;i++)
-// {
-//   for(let j = i+1; j<= arr_test1.length;j++){
-//   if(arr_test1[i] > arr_test1[j])
-// {
-//   a=arr_test1[j];
-// }
-// else if(arr_test1[i] < arr_test1[j])
-// {
-//  b = arr_test1[j]
-// }
-// }
-// }
-// console.log(a)
-// console.log(b)
+};
+}
+create_cards();
