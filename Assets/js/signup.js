6c75fedcb278bd6824feba441b79e5aa7fdd6076
Sharon2408 <sharonantoy2408@gmail.com>
2023-05-26 17:14:34 +0530

    Friday commit 26-05-23

--- Assets/js/signup.js
+++ Assets/js/signup.js
@@ -37,6 +37,7 @@ function validate_name() {
 
 // Email Validation
 function validate_email() {
+  // SignUp email validate on input
   const emailInput = document.getElementById("email").value;
   const emailregex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!emailInput) {
@@ -126,7 +127,7 @@ function load_cards() {
         card += "<h5 class='card-title'>" + object["Price"] + "</h5>";
         card += '<p class="card-text">' + object["Description"] + "</p>";
         card +=
-          '<a href="#" class="btn btn-primary glowing-button darkButton smallButton">Buy Now</a>' + '&nbsp&nbsp';
+          '<a href="#" class="btn btn-primary glowing-button darkButton smallButton" onclick="Add_to_Cart(' + object["id"] + ');" >Buy Now</a>' + '&nbsp&nbsp';
         card += '<a href="#" class="btn btn-primary glowing-button darkButton smallButton" onclick="addto_favourites(' + object["id"] + ');">favorite +</a>'
         card +=
           '<address class="card-foot"><br><i class="fa-solid fa-location-dot "></i>' +
@@ -142,6 +143,111 @@ function load_cards() {
 };
 load_cards();
 
+// BUY NOW
+
+function Add_to_Cart(id) {
+  const Cart = [];
+  const xhttp = new XMLHttpRequest();
+  xhttp.open("GET", `http://localhost:3000/Cards/${id}`);
+  xhttp.send();
+
+  xhttp.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      console.log(id);
+      const itemDetails = JSON.parse(this.responseText);
+      console.log(itemDetails);
+
+      const xhttpAdd = new XMLHttpRequest();
+
+      xhttpAdd.open("POST", "http://localhost:3000/Cart");
+      xhttpAdd.setRequestHeader("Content-Type", "application/json");
+      console.log("hello123")
+      xhttpAdd.onload = function () {
+
+
+        if (xhttpAdd.status == 200 && xhttpAdd.readystate == 4) {
+          console.log("hello");
+          console.log("hello555")
+          const response = JSON.parse(xhttpAdd.responseText);
+          Cart.push(itemDetails);
+          load_cards();
+
+        }
+      };
+      xhttpAdd.send(JSON.stringify(itemDetails));
+
+
+    }
+  }
+
+}
+
+function Buy_Now() {
+  var Cart = [];
+  const xhttp = new XMLHttpRequest();
+  xhttp.open("GET", "http://localhost:3000/Cart");
+  xhttp.send();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      // console.log(this.responseText);
+      var card = "";
+      const objects = JSON.parse(this.responseText);
+      for (let object of objects) {
+        card += '<div  class="col-md-4">';
+        card += '<div  class="card mb-4 glowing-border">';
+        card +=
+          '<img src="' +
+          object["Photo"] +
+          '" class="card-img-top" alt="Architecture 1">';
+        card += '<div class="card-body">';
+        card += "<h5 class='card-title'>" + object["Price"] + "</h5>";
+        card += '<p class="card-text"><b>' + object["Description"] + "</b></p>";
+        card +=
+          '<a href="index.html" class="btn btn-primary glowing-button darkButton smallButton" >Buy Now</a>' + '&nbsp&nbsp';
+        card += '<a href="#" class="btn btn-primary glowing-button darkButton smallButton" onclick="remove_from_Cart(' + object["id"] + ');">Remove</a>'
+        card +=
+          '<address class="card-foot"><br><i class="fa-solid fa-location-dot"></i>' +
+          object["Location"] +
+          "</address>";
+        card += "</div>";
+        card += "</div>";
+        card += "</div>";
+      }
+      if (card == '') {
+        card += '<div class="container">'
+        card += '<div class="row" id="hidefavourite">'
+        card += '   <div class="col">'
+        card += ' <h1>My Cart</h1>'
+        card += ' <p class="empty-message">You havent added anything to your Cart.</p>'
+        card += '  <a href="index.html#cards" class="btn btn-primary glowing-button">Buy Now</a>'
+        card += ' </div>'
+        card += ' </div>'
+        card += ' </div> '
+      }
+      $("#Buycards").html(card);
+    }
+  };
+};
+Buy_Now();
+
+function remove_from_Cart(id) {
+  console.log(id);
+  const xhttp = new XMLHttpRequest();
+  xhttp.open(`DELETE`, `http://localhost:3000/Cart/${id}`);
+  xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhttp.send(
+    JSON.stringify({ id: id }));
+  xhttp.onreadystatechange = () => {
+    console.log(id);
+    if (xhttp.readyState == 4) {
+      const objects = JSON.parse(xhttp.responseText);
+
+
+    }
+
+  };
+  load_cards();
+}
 
 create_cards = () => {
   const price = document.getElementById('price').value;
@@ -166,6 +272,7 @@ create_cards = () => {
     }
   };
 }
+
 
 
 function addto_favourites(id) {
@@ -225,7 +332,7 @@ favourite_cards = () => {
         card += "<h5 class='card-title'>" + object["Price"] + "</h5>";
         card += '<p class="card-text"><b>' + object["Description"] + "</b></p>";
         card +=
-          '<a href="#" class="btn btn-primary glowing-button darkButton smallButton" >Buy Now</a>' + '&nbsp&nbsp';
+          '<a href="#" class="btn btn-primary glowing-button darkButton smallButton" onclick=Add_to_Cart(' + object["id"] + ') >Buy Now</a>' + '&nbsp&nbsp';
         card += '<a href="#" class="btn btn-primary glowing-button darkButton smallButton" onclick="remove_favourites(' + object["id"] + ');">Remove</a>'
         card +=
           '<address class="card-foot"><br><i class="fa-solid fa-location-dot"></i>' +
@@ -241,7 +348,7 @@ favourite_cards = () => {
         card += '   <div class="col">'
         card += ' <h1>My Favorites</h1>'
         card += ' <p class="empty-message">You havent added anything to favorites yet.</p>'
-        card += '  <a href="/Assets/index.html#cards" class="btn btn-primary glowing-button">Add Now</a>'
+        card += '  <a href="index.html#cards" class="btn btn-primary glowing-button">Add Now</a>'
         card += ' </div>'
         card += ' </div>'
         card += ' </div> '
@@ -312,54 +419,50 @@ function validate_login() {
           updateRequest.send(
             JSON.stringify({
 
-              name: object['username'],
+              name: object['name'],
               email: object['email'],
               password: object['password'],
               confirm_password: object['confirm_password'],
-              Logged:log,
+              Logged: log,
             })
           );
-          
-          updateRequest.send(JSON.stringify({
-            Logged: log,
-          }));
-          
-          $(document).ready(function () {
-            $("#myModalButton").click(function () {
-              $("#exampleModal").modal("hide");
-            });
-          });
+          const xhttpValid = new XMLHttpRequest();
+          xhttpValid.open("GET", `http://localhost:3000/Signup`);
+          xhttpValid.send();
+          xhttpValid.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+              console.log(this.responseText);
+              const objects = JSON.parse(this.responseText);
+              var card = '';
+              for (let object of objects) {
+                if (object['Logged'] = 1) {
 
-          card +=
-            '<div class="alert alert-warning alert-dismissible fade show" role="alert">';
-          card += "<strong>Welcome!" + object["name"] + "</strong>";
-          card +=
-            ' <button type="button" class="close" data-dismiss="alert" data-bs-dismiss="modal" aria-label="Close">';
-          card += '   <span aria-hidden="true">&times;</span>';
-          card += "  </button>";
-          card += "</div>";
-          document.getElementById("alert").innerHTML = card;
-          count + 1;
-          // Hide the alert after 3 seconds
-          setTimeout(function () {
-            $("#alert").show();
-          }, 5000);
-        } 
-        else {
-          card +=
-            '<div class="alert alert-warning alert-dismissible fade show" role="alert">';
-          card += "<strong>Invalid Credentials</strong>";
-          card +=
-            ' <button type="button" class="close" data-dismiss="alert" aria-label="Close">';
-          card += '   <span aria-hidden="true">&times;</span>';
-          card += "  </button>";
-          card += "</div>";
-          document.getElementById("alert").innerHTML = card;
-          setTimeout(function () {
-            $("#alert").hide();
-          }, 5000);
+                   window.open("http://127.0.0.1:5500/Assets/index.html#")
+                   $("#loginbutton").hide();
+                    
+                  break;
+                }
+
+                else {
+                  document.getElementById("alert").innerHTML = card;
+                  $(document).ready(function () {
+                    $("#loginbutton").empty();
+                  });
+                }
+              }
+            }
+          };
+          break;
+          
+        }
+        else{
+
         }
       }
     }
-  };
+  }
+}
+
+function hide_buttton(){
+  
 }
